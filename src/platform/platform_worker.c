09a47c8e960f6681910ec1744514415b91118f2e
arun <arun.sfc@outlook.com>
2025-02-14 01:36:54 -0800

    add platform_worker ff_callback

--- src/platform/platform_worker.c
+++ src/platform/platform_worker.c
@@ -15,6 +15,8 @@ Abstract:
 #include "platform_worker.c.clog.h"
 #endif
 
+#include "ff_api.h"
+
 typedef struct QUIC_CACHEALIGN CXPLAT_WORKER {
 
     //
@@ -275,6 +277,20 @@ CxPlatRunExecutionContexts(
 //
 #define CXPLAT_WORKER_IDLE_WORK_THRESHOLD_COUNT 10
 
+int ff_platform_worker_callback(void *Context)
+{
+    CXPLAT_WORKER* Worker = (CXPLAT_WORKER*)Context;
+
+    uint32_t WaitTime = UINT32_MAX;
+
+    if (Worker->DatapathEC) {
+        if (CxPlatDataPathRunEC(&Worker->DatapathEC, Worker->ThreadId, WaitTime)) {
+        }
+    }
+
+    return 0;
+}
+
 CXPLAT_THREAD_CALLBACK(CxPlatWorkerThread, Context)
 {
     CXPLAT_WORKER* Worker = (CXPLAT_WORKER*)Context;
@@ -287,46 +303,49 @@ CXPLAT_THREAD_CALLBACK(CxPlatWorkerThread, Context)
 
     Worker->ThreadId = CxPlatCurThreadID();
 
-    uint32_t NoWorkCount = 0;
-    while (Worker->Running) {
+    ff_run(ff_platform_worker_callback, Worker);
 
-        uint32_t WaitTime = UINT32_MAX;
-        ++NoWorkCount;
-
-#ifdef QUIC_USE_EXECUTION_CONTEXTS
-        uint64_t TimeNow = CxPlatTimeUs64();
-        if (CxPlatRunExecutionContexts(Worker, &TimeNow)) {
-            NoWorkCount = 0;
-        }
-        if (Worker->ECsReady) {
-            WaitTime = 0;
-        } else if (Worker->ECsReadyTime != UINT64_MAX) {
-            uint64_t Diff = Worker->ECsReadyTime - TimeNow;
-            Diff = US_TO_MS(Diff);
-            if (Diff == 0) {
-                WaitTime = 1;
-            } else if (Diff < UINT32_MAX) {
-                WaitTime = (uint32_t)Diff;
-            } else {
-                WaitTime = UINT32_MAX-1;
-            }
-        }
-#endif
-
-        if (Worker->DatapathEC) {
-            if (CxPlatDataPathRunEC(&Worker->DatapathEC, Worker->ThreadId, WaitTime)) {
-                NoWorkCount = 0;
-            }
-        } else if (WaitTime != 0) {
-            CxPlatEventWaitWithTimeout(Worker->WakeEvent, WaitTime);
-            NoWorkCount = 0;
-        }
-
-        if (NoWorkCount > CXPLAT_WORKER_IDLE_WORK_THRESHOLD_COUNT) {
-            CxPlatSchedulerYield();
-            NoWorkCount = 0;
-        }
-    }
+//    uint32_t NoWorkCount = 0;
+//    while (Worker->Running) {
+//
+//        uint32_t WaitTime = UINT32_MAX;
+//        ++NoWorkCount;
+//
+//#ifdef QUIC_USE_EXECUTION_CONTEXTS
+//        uint64_t TimeNow = CxPlatTimeUs64();
+//        if (CxPlatRunExecutionContexts(Worker, &TimeNow)) {
+//            NoWorkCount = 0;
+//        }
+//        if (Worker->ECsReady) {
+//            WaitTime = 0;
+//        } else if (Worker->ECsReadyTime != UINT64_MAX) {
+//            uint64_t Diff = Worker->ECsReadyTime - TimeNow;
+//            Diff = US_TO_MS(Diff);
+//            if (Diff == 0) {
+//                WaitTime = 1;
+//            } else if (Diff < UINT32_MAX) {
+//                WaitTime = (uint32_t)Diff;
+//            } else {
+//                WaitTime = UINT32_MAX-1;
+//            }
+//        }
+//#endif
+//
+//        if (Worker->DatapathEC) {
+//            if (CxPlatDataPathRunEC(&Worker->DatapathEC, Worker->ThreadId, WaitTime)) {
+//                NoWorkCount = 0;
+//            }
+//        }
+//        //else if (WaitTime != 0) {
+//        //    CxPlatEventWaitWithTimeout(Worker->WakeEvent, WaitTime);
+//        //    NoWorkCount = 0;
+//        //}
+//
+//        //if (NoWorkCount > CXPLAT_WORKER_IDLE_WORK_THRESHOLD_COUNT) {
+//        //    CxPlatSchedulerYield();
+//        //    NoWorkCount = 0;
+//        //}
+//    }
 
     QuicTraceLogInfo(
         PlatformWorkerThreadStop,
